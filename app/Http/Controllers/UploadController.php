92229676cc55f2b47bc1d57df8b19862302c5999
electrospin <ffq369@mocs.utc.edu>
2017-11-29 23:36:16 -0500

    Forgot to commit the UploadController.php

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -7,16 +7,15 @@ use Storage;
 class UploadController extends Controller
 {
     public function upload(Request $request){
+
         if($request->hasFile('file')){
 
             $files = $request->file('file');
             foreach ($files as $file):
                 Storage::put($file->getClientOriginalName(), file_get_contents($file));
             endforeach;
-
-           // return $request->file->storeAs('public/upload',$filename);
-
-            //echo '<img src="uploads/'. $file->getClientOriginalName();
+           /* return $request->file->storeAs('public/upload',$filename);
+            echo '<img src="uploads/'. $file->getClientOriginalName();*/
             return \Response::json(array('success' => true));
         }
 
